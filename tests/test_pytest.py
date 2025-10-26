cebcb3c2f79bfeb7031634536fb21940a03c4c35
Konstantin <maschkovc@yandex.ru>
2025-10-26 12:39:37 +0300

    pyutest add

---
+++ tests/test_pytest.py
@@ -0,0 +1,15 @@
+def test_user_login():
+    print('test')
+
+class TestUserLogin:
+    def test_1(self):
+        ...
+
+    def test_2(self):
+        ...    
+        
+def test_assert_positive():
+    assert(2 + 2) == 4        
+    
+def test_assert_negative():
+    assert(2 + 2) == 5, "(2 + 2) != 5"            
