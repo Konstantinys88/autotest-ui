477d1089bfd8d33c783cc93acf98c0c346de47cf
Konstantin <maschkovc@yandex.ru>
2025-10-27 22:38:54 +0300

    fixture

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,34 +1,54 @@
 import pytest
 
-def test_user_login():
-    print('test')
+# @pytest.fixture(autouse=True)
+# def analitics_data():
+#     print('Анализ данных')
+    
+# @pytest.fixture(scope='session')    
+# def settings():
+#     print('session')
+    
+# @pytest.fixture(scope='class')    
+# def settings2():
+#     print('class')    
+    
+@pytest.fixture
+def fixture_test():
+    print('fixture_test')    
+        
+    
 
 class TestUserLogin:
+    @pytest.mark.test_1
     def test_1(self):
         ...
-
+        
+        
+    @pytest.mark.test_2    
     def test_2(self):
         ...    
+        
+class TestUserLogin2:
+    @pytest.mark.test_1
+    def test_3(self):
+        ...
+        
+        
+    @pytest.mark.test_2    
+    def test_4(self, fixture_test):
+        print('test 4')           
         
 def test_assert_positive():
     assert(2 + 2) == 4        
     
-# def test_assert_negative():
-    # assert(2 + 2) == 5, "(2 + 2) != 5"            
+@pytest.mark.skip(reason="Всё пропало")    
+def test_assert_negative():
+    assert(2 + 2) == 5, "(2 + 2) != 5"            
     
     
     
     
-@pytest.mark.test_1
-def test_1():
-    print('test_1')
-    
-@pytest.mark.test_1
-@pytest.mark.test_2     
-def test_2():
-    print('test_2')
-       
-@pytest.mark.test_3       
-def test_3():
-    print('test_3')
+
+
+    result = divide(10, 2)    
            
