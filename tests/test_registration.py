f829c6aed8099135f31e5385ff680949254244c9
Konstantin <maschkovc@yandex.ru>
2025-10-28 20:44:20 +0300

    fixture

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,34 +1,48 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect, Page
 
+
+@pytest.mark.regression
+@pytest.mark.authorization
+# Использование фикстуры 'chromium_page', которая автоматически предоставляет готовую страницу
+def test_wrong_email_or_password_authorization(chromium_page: Page):
+    # Теперь страница передаётся в тест через фикстуру 'chromium_page', браузер не нужно инициализировать вручную
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
+
+    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
+    password_input.fill("password")
+
+    login_button = chromium_page.get_by_test_id('login-page-login-button')
+    login_button.click()
+
+    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+    
+import pytest
+from playwright.sync_api import expect, Page
+
+
+@pytest.mark.regression
 @pytest.mark.registration
-def test_registration():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+def test_successful_registration(chromium_page: Page):  # Теперь используем фикстуру
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
 
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
+    username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
 
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
+    password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
 
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
+    registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
 
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
+    dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title).to_be_visible()
 
-        context.storage_state(path="browser-state.json")
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='browser-state.json')
-        page = context.new_page()
-
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
-
-        page.wait_for_timeout(3000)
