1367490eba5c8d5aefccac58df7ceb17f8d2cd0b
Konstantin <maschkovc@yandex.ru>
2025-11-15 22:39:18 +0300

    allure step

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -10,6 +10,7 @@ from tools.allure.tags import AllureTag
 from tools.allure.epics import AllureEpick
 from tools.allure.stories import AllureStories
 from tools.allure.features import AllureFeature
+from allure_commons.types import Severity
 
 
 
@@ -21,9 +22,12 @@ from tools.allure.features import AllureFeature
 @allure.feature(AllureFeature.COURSES)
 @allure.story(AllureStories.STORIES)
 @allure.tag(AllureTag.REGRESSION)
+@allure.severity(Severity.BLOCKER)
 def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
-    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
-    create_course_page.check_visible_create_course_title()
+    with allure.step('Open: https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create'):
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+    with allure.step('проверка заголовка'):
+        create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
     create_course_page.check_visible_image_preview_empty_view()
     create_course_page.check_visible_image_upload_view()
